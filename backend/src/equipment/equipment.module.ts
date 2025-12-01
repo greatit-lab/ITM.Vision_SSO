adc0338f00a9061bcb0d360aea806b1a37547037
greatit-lab <gil2zum@gmail.com>
2025-12-01 17:29:21 +0900

    Update equipment.module.ts -- 1201_17

--- backend/src/equipment/equipment.module.ts
+++ backend/src/equipment/equipment.module.ts
@@ -1,10 +1,14 @@
-// src/equipment/equipment.module.ts
+// backend/src/equipment/equipment.module.ts
 import { Module } from '@nestjs/common';
 import { EquipmentController } from './equipment.controller';
 import { EquipmentService } from './equipment.service';
+import { PrismaService } from '../prisma.service'; // [추가] PrismaService 임포트
 
 @Module({
   controllers: [EquipmentController],
-  providers: [EquipmentService],
+  providers: [
+    EquipmentService,
+    PrismaService, // [추가] 이 모듈의 Provider 목록에 PrismaService 등록
+  ],
 })
 export class EquipmentModule {}
