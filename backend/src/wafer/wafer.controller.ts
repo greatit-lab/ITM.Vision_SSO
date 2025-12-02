1e6b510880d08cae8be27e30fe0c762551f0081c
greatit-lab <gil2zum@gmail.com>
2025-12-02 13:26:13 +0900

    Update wafer.controller.ts -- 1202_13

--- backend/src/wafer/wafer.controller.ts
+++ backend/src/wafer/wafer.controller.ts
@@ -24,4 +24,11 @@ export class WaferController {
   checkPdf(@Query() query: WaferQueryParams) {
     return this.waferService.checkPdf(query);
   }
+
+  // ▼▼▼ [수정] PDF 이미지 엔드포인트 구현 ▼▼▼
+  @Get('pdfimage')
+  async getPdfImage(@Query() query: WaferQueryParams) {
+    // 서비스에서 Base64 문자열을 받아 클라이언트에 그대로 반환
+    return await this.waferService.getPdfImage(query);
+  }
 }
