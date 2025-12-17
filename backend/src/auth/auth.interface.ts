3174fc9d8de706180aaa2416e4ce1e15d4c5ee4a
greatit-lab <gil2zum@gmail.com>
2025-12-17 19:11:54 +0900

    Update auth.interface.ts --1217_19

--- backend/src/auth/auth.interface.ts
+++ backend/src/auth/auth.interface.ts
@@ -3,12 +3,14 @@ export interface User {
   userId: string;
   email: string;
   name: string;
+  department: string;
+  companyCode: string; // [추가] 회사 코드
   groups: string | string[];
   sessionIndex?: string;
-  department: string;
 }
 
+// 로그인 결과에 Role 정보 추가
 export interface LoginResult {
   access_token: string;
-  user: User;
+  user: User & { role: string }; // [수정] role 포함
 }
