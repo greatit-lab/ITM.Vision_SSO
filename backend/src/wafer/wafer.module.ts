73b92583c21e5fbad0d3de76f24556b3f24eddbf
greatit-lab <gil2zum@gmail.com>
2025-12-18 18:31:22 +0900

    Create wafer.module.ts --1218_18

---
+++ backend/src/wafer/wafer.module.ts
@@ -0,0 +1,11 @@
+// backend/src/wafer/wafer.module.ts
+import { Module } from '@nestjs/common';
+import { WaferController } from './wafer.controller';
+import { WaferService } from './wafer.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [WaferController],
+  providers: [WaferService, PrismaService],
+})
+export class WaferModule {}
