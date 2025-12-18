ad66d870f8f1c25b05bde3d55ee04fc6591faa9c
greatit-lab <gil2zum@gmail.com>
2025-12-18 20:03:21 +0900

    Update error.controller.ts --1218_20

--- backend/src/error/error.controller.ts
+++ backend/src/error/error.controller.ts
@@ -1,7 +1,11 @@
+// backend/src/error/error.controller.ts
 import { Controller, Get, Query } from '@nestjs/common';
 import { ErrorService } from './error.service';
 
-@Controller('api/ErrorAnalytics')
+// [수정] main.ts의 Global Prefix('api')와 중복되지 않도록 'api/' 제거
+// 변경 전: @Controller('api/ErrorAnalytics')
+// 변경 후: @Controller('ErrorAnalytics')
+@Controller('ErrorAnalytics') 
 export class ErrorController {
   constructor(private readonly errorService: ErrorService) {}
 
