a7cc7e726fc061399f87cd417e7d4d7c92f1c593
greatit-lab <gil2zum@gmail.com>
2025-12-22 19:06:33 +0900

    Update infra.controller.ts --1222_19

--- backend/src/infra/infra.controller.ts
+++ backend/src/infra/infra.controller.ts
@@ -7,10 +7,9 @@ import {
   Delete,
   Body,
   Param,
-  ParseIntPipe,
 } from '@nestjs/common';
 import { InfraService } from './infra.service';
-import { Prisma } from '@prisma/client'; // [추가] Prisma 타입 가져오기
+import { Prisma } from '@prisma/client';
 
 @Controller('infra')
 export class InfraController {
@@ -22,31 +21,6 @@ export class InfraController {
     return await this.infraService.getSdwts();
   }
 
-  // 2. Infra Server List (cfg_infra_server)
-  @Get('server-list')
-  async getInfraServers() {
-    return await this.infraService.getInfraServers();
-  }
-
-  @Post('server-list')
-  async createInfraServer(@Body() body: Prisma.CfgInfraServerCreateInput) {
-    // any -> Prisma.CfgInfraServerCreateInput 으로 변경
-    return await this.infraService.createInfraServer(body);
-  }
-
-  @Put('server-list/:id')
-  async updateInfraServer(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() body: Prisma.CfgInfraServerUpdateInput, // any -> Prisma.CfgInfraServerUpdateInput 으로 변경
-  ) {
-    return await this.infraService.updateInfraServer(id, body);
-  }
-
-  @Delete('server-list/:id')
-  async deleteInfraServer(@Param('id', ParseIntPipe) id: number) {
-    return await this.infraService.deleteInfraServer(id);
-  }
-
   // 3. Agent Server (Per Eqp Config)
   @Get('agent-server')
   async getAgentServers() {
@@ -55,14 +29,13 @@ export class InfraController {
 
   @Post('agent-server')
   async createAgentServer(@Body() body: Prisma.CfgServerCreateInput) {
-    // any -> Prisma.CfgServerCreateInput 으로 변경
     return await this.infraService.createAgentServer(body);
   }
 
   @Put('agent-server/:id')
   async updateAgentServer(
     @Param('id') id: string,
-    @Body() body: Prisma.CfgServerUpdateInput, // any -> Prisma.CfgServerUpdateInput 으로 변경
+    @Body() body: Prisma.CfgServerUpdateInput,
   ) {
     return await this.infraService.updateAgentServer(id, body);
   }
